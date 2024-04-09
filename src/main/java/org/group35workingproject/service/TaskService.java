64b15398eea85ea2bbb3e89e0e00b6673574b509
StanislavPB <perminovsb@gmail.com>
2024-04-09 17:18:37 +0100

    converters

--- src/main/java/org/group35workingproject/service/TaskService.java
+++ src/main/java/org/group35workingproject/service/TaskService.java
@@ -1,15 +1,41 @@
 package org.group35workingproject.service;
 
 import lombok.AllArgsConstructor;
+import org.group35workingproject.domain.Task;
+import org.group35workingproject.dto.taskDto.TaskResponseDTO;
 import org.group35workingproject.repository.TaskRepository;
+import org.group35workingproject.service.exception.NotFoundException;
+import org.group35workingproject.service.util.TaskConverter;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class TaskService {
 
     private final TaskRepository taskRepository;
+    private final TaskConverter converter;
 
+    public List<TaskResponseDTO> findAll(){
+//        List<Task> tasks = taskRepository.findAll();
+//        List<TaskResponseDTO> dtos = new ArrayList<>();
+//        for (Task task : tasks) {
+//            TaskResponseDTO dto = converter.toDto(task);
+//            dtos.add(dto);
+//        }
+//
+//        return dtos;
 
-   // public List<> findAll(){}
+        return taskRepository.findAll().stream()
+                .map(converter::toDto)
+                .toList();
+    }
+
+    public TaskResponseDTO findById(Integer id) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Task with id = " + id + " not found"));
+        return converter.toDto(task);
+    }
 }
---
+++ src/main/java/org/group35workingproject/service/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package org.group35workingproject.service.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
