0e490d93cd36cfd9317fb7b93f92115f18093443
StanislavPB <perminovsb@gmail.com>
2024-04-10 19:48:14 +0100

    add validation

--- src/main/java/org/group35workingproject/controller/ManagerController.java
+++ src/main/java/org/group35workingproject/controller/ManagerController.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import org.group35workingproject.dto.managerDto.ManagerCreateRequestDTO;
 import org.group35workingproject.dto.managerDto.ManagerCreateResponseDTO;
 import org.group35workingproject.service.ManagerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,10 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/managers")
 public class ManagerController {
 
+    private static final Logger log = LoggerFactory.getLogger(ManagerController.class);
     private final ManagerService managerService;
 
     @PostMapping
     public ResponseEntity<ManagerCreateResponseDTO> createManager(@RequestBody ManagerCreateRequestDTO request) {
+        //log.info("Start controller createManager"); - плохой вариант
         return new ResponseEntity<>(managerService.createManager(request), HttpStatus.CREATED);
     }
 }
