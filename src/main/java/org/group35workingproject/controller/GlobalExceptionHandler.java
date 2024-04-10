86782f90dc11df7fa1e193f0a6ba76972a819064
StanislavPB <perminovsb@gmail.com>
2024-04-10 17:31:52 +0100

    add handlerException

---
+++ src/main/java/org/group35workingproject/controller/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package org.group35workingproject.controller;
+
+import org.group35workingproject.service.exception.AlreadyExistException;
+import org.group35workingproject.service.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<String> handlerNullPointerException(NullPointerException exception) {
+        return new ResponseEntity<>(exception.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+   @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handlerNotFoundException(NotFoundException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+   @ExceptionHandler(AlreadyExistException.class)
+    public ResponseEntity<String> handlerAlreadyExistException(AlreadyExistException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
